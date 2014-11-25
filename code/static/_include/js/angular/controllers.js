0296e4362079d5b4c524bc0b74778267ff945193
Rafol <rafael.salgado@ventura24.es>
2014-11-25 19:34:28 +0100

    Added voting

--- code/static/_include/js/angular/controllers.js
+++ code/static/_include/js/angular/controllers.js
@@ -4,11 +4,19 @@ var v24App = angular.module('v24App', []);
 
 v24App.controller('workersCtrl', function ($scope, $http) {
   $http.get('data/data.json').success(function(data) {
-  	
     $scope.workers = data;
   }).
   error(function(data, status, headers, config) {
     alert("error")
   });
+  $scope.getNumber = function(num) {
+
+	  return new Array(num);   
+  };
+  $scope.vote = function(id,score) {
+  	 $http.get('/vote/rate/'+id+"/"+score+"/",
+        function(response) { console.log(response); },
+        function(failure) { console.log("failed :(", failure); });
+  };
 
 });
