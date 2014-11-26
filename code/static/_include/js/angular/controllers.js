c9efe4de4a7cb096ed3f2580dae11ae62a7e85b7
moncho <ramon.crespo@ventura24.es>
2014-11-26 19:08:04 +0100

    Added two more data files. Added more description to index. Tried to create a way to know if voting is opened.

--- code/static/_include/js/angular/controllers.js
+++ code/static/_include/js/angular/controllers.js
@@ -3,25 +3,50 @@ var v24App = angular.module('v24App', []);
 
 
 v24App.controller('workersCtrl', function ($scope, $http) {
-  $http.get('data/data.json').success(function(data) {
+  $http.get('/data/data.json').success(function(data) {
     $scope.workers = data;
   }).
   error(function(data, status, headers, config) {
-    alert("error")
+    console.log("error loading workers")
   });
-  $scope.getNumber = function(num) {
 
+  $scope.canVote = function() {
+    canVote = false;
+    $http.get('/vote/status/').success(function(data) {
+     canVote = data.status == "open";
+    });
+    return canVote;
+  };
+
+  $scope.getNumber = function(num) {
 	  return new Array(num);   
   };
-  $scope.vote = function(id,score){
-  	 
 
-     $http.get('/vote/rate/'+id+'/'+score+'/').
-  success(function(data, status, headers, config) {
-   console.log("exito: "+data);
+  $scope.vote = function(id,score) {
+    $http.get('/vote/rate/'+id+'/'+score+'/').
+      success(function(data, status, headers, config) {
+        console.log("exito: "+data);
+      }).
+      error(function(data, status, headers, config) {
+        console.log("error: "+data);
+      });
+  }
+});
+
+v24App.controller('file_workersCtrl', function ($scope, $http) {
+  $http.get('/data/filemix_data.json').success(function(data) {
+    $scope.workers = data;
   }).
   error(function(data, status, headers, config) {
-    console.log("error: "+data);
+    console.log("error loading fileworkers")
   });
-  }
+});
+
+v24App.controller('it_workersCtrl', function ($scope, $http) {
+  $http.get('/data/italianmix_data.json').success(function(data) {
+    $scope.workers = data;
+  }).
+  error(function(data, status, headers, config) {
+    console.log("error loading fileworkers")
+  });
 });
