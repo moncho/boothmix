a3d3537b3a31f73410841b4d1d66128e9b88976e
Rafael Salagado Almazán <rafaelsalagadoalmazan@iMac-de-Rafael-Salagado-Almazan.local>
2014-11-26 03:10:48 +0100

    Adding hiding stars after voting

--- code/static/_include/js/angular/controllers.js
+++ code/static/_include/js/angular/controllers.js
@@ -13,10 +13,15 @@ v24App.controller('workersCtrl', function ($scope, $http) {
 
 	  return new Array(num);   
   };
-  $scope.vote = function(id,score) {
-  	 $http.get('/vote/rate/'+id+"/"+score+"/",
-        function(response) { console.log(response); },
-        function(failure) { console.log("failed :(", failure); });
-  };
+  $scope.vote = function(id,score){
+  	 
 
+     $http.get('/vote/rate/'+id+'/'+score+'/').
+  success(function(data, status, headers, config) {
+   console.log("exito: "+data);
+  }).
+  error(function(data, status, headers, config) {
+    console.log("error: "+data);
+  });
+  }
 });
